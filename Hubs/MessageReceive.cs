1b690af6ab472bf91fbd26e7a6dc3c43e62882a2
p4nd4ta <stu1901681077@uni-plovdiv.bg>
2021-02-27 15:24:23 +0200

    Make sure that JSON .NET Deserialization Attacks are prevented. Before they were too, but just in case

--- Hubs/MessageReceive.cs
+++ Hubs/MessageReceive.cs
@@ -8,13 +8,13 @@ namespace ChatAppProject.Hubs
 {
     public class MessageReceive//This class holds the properties of messages sent from client to server in JSON deserialized fashion
     {
-        [JsonProperty("message")]//Deserialize JSON Automatically
+        [JsonProperty("message", ItemTypeNameHandling = TypeNameHandling.None, TypeNameHandling = TypeNameHandling.None)]//Deserialize JSON Automatically
         public string message { get; set; } //the body of the message(encrypted with public key and base64 encoded from client side)
 
-        [JsonProperty("signedMessage")]
+        [JsonProperty("signedMessage", ItemTypeNameHandling = TypeNameHandling.None, TypeNameHandling = TypeNameHandling.None)]
         public string signedMessage { get; set; } //SHA-256 hash of the message, encrypted with the user's private key for authenticity(crypthographic signing)
 
-        [JsonProperty("recipientId")]
+        [JsonProperty("recipientId", ItemTypeNameHandling = TypeNameHandling.None, TypeNameHandling = TypeNameHandling.None)]
         public string recipientId { get; set; } //The ID of the user it is meant to(and encrypted with his/hers public key)
     }
 }
